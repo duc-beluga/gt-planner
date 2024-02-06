fa1a893847a8e0379aab8474f9fd950f4834b7f5
Duc Nguyen <dnguyen416@gatech.edu>
2024-02-06 16:45:53 -0500

    style: Home

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,8 +2,12 @@ import React from "react";
 
 const Home = () => {
   return (
-    <div className="flex justify-center items-center h-full text-5xl text-white">
+    <div className="flex gap-4 flex-col justify-center items-center h-full text-5xl text-white">
       This is Home Page
+      <div className="flex gap-2">
+        <button className="border-2 p-2">Button 1</button>
+        <button className="border-2 p-2">Button 2</button>
+      </div>
     </div>
   );
 };
