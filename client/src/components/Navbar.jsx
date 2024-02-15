e5fde4d9a4b74ffaf011b8ac4cb67c4a0226de19
Duc Nguyen <dnguyen416@gatech.edu>
2024-02-15 13:21:33 -0500

    style: navbar elements as menu

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -5,26 +5,37 @@ import { useAuth } from "../context/AuthContext";
 const Navbar = () => {
   const { logOut, currentUser } = useAuth();
   return (
-    <div className="navbar justify-between shadow-2xl top-0 bg-white gap-x-4">
-      <div>
-        <Link to="/" className="p-2">
-          Home
-        </Link>
-        {currentUser && (
-          <Link to="/user" className="p-2">
-            User
-          </Link>
-        )}
+    <div className="navbar shadow-2xl top-0 bg-white gap-x-4 px-4">
+      <div className="navbar-start">
+        <ul className="menu menu-horizontal rounded-box">
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          {currentUser && (
+            <li>
+              <Link to="/user">User</Link>
+            </li>
+          )}
+          {currentUser && (
+            <li>
+              <Link to="/user">Saved</Link>
+            </li>
+          )}
+        </ul>
       </div>
-      {currentUser ? (
-        <button onClick={logOut} className="p-2">
-          Log Out
-        </button>
-      ) : (
-        <Link to="/" className="p-2">
-          Log In
-        </Link>
-      )}
+      <div className="navbar-end">
+        <ul className="menu menu-horizontal rounded-box">
+          {currentUser ? (
+            <li>
+              <button onClick={logOut}>Log Out</button>
+            </li>
+          ) : (
+            <li>
+              <Link to="/">Log In</Link>
+            </li>
+          )}
+        </ul>
+      </div>
     </div>
   );
 };
