936117dce04ea4a312734157c0fe67d4fc1337a1
Duc Nguyen <dnguyen416@gatech.edu>
2024-02-15 00:05:08 -0500

    style: navbar login/logout

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -5,13 +5,15 @@ import { useAuth } from "../context/AuthContext";
 const Navbar = () => {
   const { logOut, currentUser } = useAuth();
   return (
-    <div className="navbar shadow-2xl top-0 bg-white">
-      <Link to="/" className="p-2">
-        Home
-      </Link>
-      <Link to="/user" className="p-2">
-        User
-      </Link>
+    <div className="navbar justify-between shadow-2xl top-0 bg-white gap-x-4">
+      <div>
+        <Link to="/" className="p-2">
+          Home
+        </Link>
+        <Link to="/user" className="p-2">
+          User
+        </Link>
+      </div>
       {currentUser ? (
         <button onClick={logOut} className="p-2">
           Log Out
