d532a170338e1f0c1093c1e8f7b193ea31d3d4e8
duc-beluga <dnguyen416@gatech.edu>
2024-02-18 13:36:29 -0500

    feat: rebuild plans from savedPlan

--- client/src/components/BuiltCard.jsx
+++ client/src/components/BuiltCard.jsx
@@ -1,13 +1,21 @@
 import React from "react";
 
-const BuiltCard = ({ planName }) => {
+const BuiltCard = ({ plan, setIsPlanChosen, setPlanChosen }) => {
   return (
     <div className="card w-96 bg-base-100 shadow-xl">
       <div className="card-body">
-        <h3 className="card-title">{planName}</h3>
+        <h3 className="card-title">{plan.name}</h3>
 
         <div className="card-actions justify-end">
-          <button className="btn btn-primary">Build</button>
+          <button
+            className="btn btn-primary"
+            onClick={() => {
+              setPlanChosen(JSON.parse(plan.content));
+              setIsPlanChosen(true);
+            }}
+          >
+            Build
+          </button>
         </div>
       </div>
     </div>
--- client/src/pages/SavedBuilt.jsx
+++ client/src/pages/SavedBuilt.jsx
@@ -1,12 +1,15 @@
 import React, { useEffect, useState } from "react";
 import BuiltCard from "../components/BuiltCard";
 import { useAuth } from "../context/AuthContext";
+import PlayGround from "../components/PlayGround";
 import axios from "axios";
 
 const SavedBuilt = () => {
   const [savedPlans, setSavedPlans] = useState([]);
-  const { currentUser } = useAuth();
+  const [isPlanChosen, setIsPlanChosen] = useState(false);
+  const [planChosen, setPlanChosen] = useState(null);
 
+  const { currentUser } = useAuth();
   useEffect(() => {
     axios
       .post(`${import.meta.env.VITE_SERVER_URL}/api/user/getPlans`, {
@@ -15,17 +18,27 @@ const SavedBuilt = () => {
       .then((res) => setSavedPlans(res.data))
       .catch((err) => console.log(err));
   }, []);
-  return (
+  return !isPlanChosen ? (
     <div className="flex flex-col justify-center items-center h-full text-5xl gap-y-5">
       This page contains all saved builts
       <div className="grid grid-cols-3 gap-4">
         {savedPlans.map((plan) => (
-          <BuiltCard planName={plan.name} />
+          <BuiltCard
+            key={plan.name}
+            plan={plan}
+            setIsPlanChosen={setIsPlanChosen}
+            setPlanChosen={setPlanChosen}
+          />
         ))}
         {/* <BuiltCard /> */}
         {/* <BuiltCard /> */}
       </div>
     </div>
+  ) : (
+    <PlayGround
+      initialNodes={planChosen.nodes}
+      initialEdges={planChosen.edges}
+    />
   );
 };
 
