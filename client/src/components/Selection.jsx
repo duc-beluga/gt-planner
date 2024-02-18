49c18f34bde8d234b2a844e08d00fdcb3d49933b
Maiikiru <89322078+Maiikiru@users.noreply.github.com>
2024-02-18 13:31:00 -0500

    tinkered with buttons, still cannot get button to fill button

--- client/src/components/Selection.jsx
+++ client/src/components/Selection.jsx
@@ -1,18 +1,27 @@
 import React from "react";
 import BuiltCard from "./BuiltCard";
+import { PlusCircle } from "lucide-react";
 
 const SavedBuilt = () => {
   return (
-   <main class="h-screen justify-center flex flex-col items-center">
-    <h1>How would you like to start?</h1>
-    <body class ="carousel rounded-box">
-      <div class="carousel-item">
-        <img src="https://daisyui.com/images/stock/photo-1559703248-dcaaec9fab78.jpg" alt="Burger" />
+   <main className="h-screen justify-center flex flex-col items-center">
+    <h1 className="text-2xl">How would you like to start?</h1>
+    <div className ="carousel rounded-box">
+      <div className="carousel-item">
+      <input type= "image" id=" " onclick=" " src="https://daisyui.com/images/stock/photo-1559703248-dcaaec9fab78.jpg" className="btn" />
+      {/* <button className="btn btn-outline"
+          onClick={() => {
+
+          }}
+        >
+          <img src="https://daisyui.com/images/stock/photo-1559703248-dcaaec9fab78.jpg" alt="Class graph" />
+        </button> */}
+        
       </div> 
-      <div class="carousel-item">
-        <img src="https://daisyui.com/images/stock/photo-1565098772267-60af42b81ef2.jpg" alt="Burger" />
+      <div className="carousel-item">
+        <img src="https://daisyui.com/images/stock/photo-1565098772267-60af42b81ef2.jpg" alt="Build my own" />
       </div> 
-   </body>
+   </div>
    </main>
   );
 };
