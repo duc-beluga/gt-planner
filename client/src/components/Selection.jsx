b31a230c7d8891fa2484e9f646fe4f01d4631ea3
Maiikiru <89322078+Maiikiru@users.noreply.github.com>
2024-02-20 19:23:13 -0500

    Added the buttons properly

--- client/src/components/Selection.jsx
+++ client/src/components/Selection.jsx
@@ -5,23 +5,36 @@ import { PlusCircle } from "lucide-react";
 const SavedBuilt = () => {
   return (
    <main className="h-screen justify-center flex flex-col items-center">
-    <h1 className="text-2xl">How would you like to start?</h1>
-    <div className ="carousel rounded-box">
+    <h1 className="text-5xl">How would you like to start?</h1>
+    <div className ="carousel carousel-center rounded-box">
       <div className="carousel-item">
-      <input type= "image" id=" " onclick=" " src="https://daisyui.com/images/stock/photo-1559703248-dcaaec9fab78.jpg" className="btn" />
-      {/* <button className="btn btn-outline"
+      <button className="btn h-max btn-outline p-0 rounded-none"
           onClick={() => {
-
+            console.log("Blue Pressed");
           }}
         >
           <img src="https://daisyui.com/images/stock/photo-1559703248-dcaaec9fab78.jpg" alt="Class graph" />
-        </button> */}
-        
+        </button>
       </div> 
       <div className="carousel-item">
-        <img src="https://daisyui.com/images/stock/photo-1565098772267-60af42b81ef2.jpg" alt="Build my own" />
+      <button className="btn h-max btn-outline p-0 rounded-none"
+          onClick={() => {
+            console.log("Pink Pressed");
+          }}
+        >
+          <img src="https://daisyui.com/images/stock/photo-1565098772267-60af42b81ef2.jpg" alt="Class graph" />
+        </button>
       </div> 
    </div>
+
+   <button
+          className="btn btn-neutral"
+          onClick={() => {
+            console.log("save pressed")
+          }}
+        >
+          Save
+        </button>
    </main>
   );
 };
