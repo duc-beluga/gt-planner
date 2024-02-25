8d5eb6968f594444fb5ee7385a97e8d3df3d47ac
Noah Nguyen <dnguyen416@gatech.edu>
2024-02-25 10:35:51 -0500

    fix: import naming convention

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,7 +4,7 @@ import Home from "./pages/Home";
 import User from "./pages/User";
 import Navbar from "./components/Navbar";
 import SavedBuilt from "./pages/SavedBuilt";
-import CoursesTaken from "./pages/coursestaken";
+import CoursesTaken from "./pages/CourseTaken";
 import Footer from "./components/Footer";
 import { Toaster } from "react-hot-toast";
 
