fa0430ee8b732f746e5c4c42e8372d9e652ae472
Noah Nguyen <dnguyen416@gatech.edu>
2024-03-01 16:50:13 -0500

    style: BuiltCard
    style: BuiltCard

--- client/src/components/BuiltCard.jsx
+++ client/src/components/BuiltCard.jsx
@@ -2,8 +2,17 @@ const BuiltCard = ({ plan, setPlanBuildChosen, setPlanDeleteChosen }) => {
   return (
     <div className="card w-96 bg-base-100 shadow-xl">
       <div className="card-body">
-        <h3 className="card-title">{plan.name}</h3>
-
+        <h3 className="card-title justify-center">{plan.name}</h3>
+        <div className="stats shadow m-3">
+          <div className="stat place-items-center">
+            <div className="stat-title">Taken</div>
+            <div className="stat-value text-primary">21</div>
+          </div>
+          <div className="stat place-items-center">
+            <div className="stat-title">Rem</div>
+            <div className="stat-value text-secondary">30</div>
+          </div>
+        </div>
         <div className="card-actions justify-between">
           <button
             className="btn btn-outline btn-error"
