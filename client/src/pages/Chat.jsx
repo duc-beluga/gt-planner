ed73fa1f8715fd687b4730f94c71755c8d1ed02e
Duc Nguyen <dnguyen416@gatech.edu>
2024-03-01 22:08:05 -0500

    fix: image url

--- client/src/pages/Chat.jsx
+++ client/src/pages/Chat.jsx
@@ -3,7 +3,10 @@ import React from "react";
 const Chat = () => {
   return (
     <div className="flex justify-center items-center">
-      <img src="public/bgImage2.png" className="rounded-lg m-4"></img>
+      <img
+        src="https://i.ibb.co/n1wVKTx/bgImage2.png"
+        className="rounded-lg m-4"
+      ></img>
     </div>
   );
 };
