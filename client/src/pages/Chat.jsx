c3b1a912683e119ab7453299f6f7ba9efe708334
Duc Nguyen <dnguyen416@gatech.edu>
2024-03-01 19:21:21 -0500

    feat: added chatPage

---
+++ client/src/pages/Chat.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Truck } from "lucide-react";
+
+const Chat = () => {
+  return (
+    <div>
+      Coming soon.. <Truck />
+    </div>
+  );
+};
+
+export default Chat;
