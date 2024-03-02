b4a1ed1c6f2e41c33b28e322a30c086ce7459673
Noah Nguyen <dnguyen416@gatech.edu>
2024-03-01 19:18:40 -0500

    style: UserPage (added Discoverable Mode)
    * feat: added discoverable mode
    
    * temp: added random credit in savedBuilt

--- client/src/pages/User.jsx
+++ client/src/pages/User.jsx
@@ -5,8 +5,18 @@ const User = () => {
   const { currentUser } = useAuth();
 
   return (
-    <div className="flex justify-center items-center h-full text-5xl">
-      Hi {currentUser.displayName}
+    <div className="flex flex-col justify-center items-center h-full gap-y-4">
+      <div className="card card-compact w-96 bg-base-100 shadow-xl">
+        <div className="card-body">
+          <h2 className="card-title">{currentUser.displayName}</h2>
+          <div className="card-actions justify-end">
+            <label className="label cursor-pointer gap-4">
+              <span className="label-text">Discoverable</span>
+              <input type="checkbox" className="toggle toggle-primary" />
+            </label>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
