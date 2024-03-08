9408cf0bf7a791b8058f7b956e6223bbbd9ecbc9
Duc Nguyen <dnguyen416@gatech.edu>
2024-03-08 10:32:38 -0500

    feat: added Suspense and ErrorBoundary

---
+++ client/src/components/ErrorBoundary.jsx
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+
+class ErrorBoundary extends Component {
+  state = { hasError: false };
+
+  static getDerivedStateFromError(error) {
+    return { hasError: true };
+  }
+
+  componentDidCatch(error, errorInfo) {
+    console.error(error, errorInfo);
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return this.props.fallback;
+    }
+
+    return this.props.children;
+  }
+}
+
+export default ErrorBoundary;
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,13 +4,20 @@ import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 import { AuthProvider } from "./context/AuthContext.jsx";
 import "./index.css";
+import ErrorBoundary from "./components/ErrorBoundary.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <AuthProvider>
-        <App />
-      </AuthProvider>
-    </BrowserRouter>
+    <ErrorBoundary
+      fallback={
+        <div className="text-5xl font-bold">"Something went wrong."</div>
+      }
+    >
+      <BrowserRouter>
+        <AuthProvider>
+          <App />
+        </AuthProvider>
+      </BrowserRouter>
+    </ErrorBoundary>
   </React.StrictMode>
 );
