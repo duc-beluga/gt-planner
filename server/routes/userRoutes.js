8796debc97158b915ed046c2eb2db7c7869f01a7
Cole Gensheimer <92688065+cgensheimer@users.noreply.github.com>
2024-03-09 16:53:23 -0500

    Refactor deletePlan Controller to Remove Dependency on req.body #13
    And routes adjustment

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -3,10 +3,21 @@ import userController from "../controllers/userController.js";
 
 const router = express.Router();
 
+// POST /users: Create a new user
 router.route("/").post(userController.createUser);
-router.route("/addPlan").post(userController.addPlanToUser);
-router.route("/deletePlan").delete(userController.deleteUserPlan);
-router.route("/getPlans").post(userController.getUserPlans);
-router.route("/updatePlan").post(userController.updatePlanUser);
+
+// GET /users/:email/plans: Get user plans
+// POST /users/:email/plans: Add plan to user
+router
+  .route("/:email/plans/")
+  .get(userController.getUserPlans)
+  .post(userController.updatePlanUser);
+
+// PUT /users/:email/plans/:planName: Update plan for user
+// DELETE /users/:email/plans/:planName: Delete plan for user
+router
+  .route("/:email/plans/:planName")
+  .put(userController.updatePlanUser)
+  .delete(userController.deleteUserPlan);
 
 export default router;
