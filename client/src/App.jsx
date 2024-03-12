93d7fb138aedacd10c4d778fbbb81a8f31d877a1
Duc Nguyen <dnguyen416@gatech.edu>
2024-03-12 10:28:05 -0400

    labor: workflow

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -26,7 +26,7 @@ function App() {
       <Navbar />
       <MainContainer>
         <Suspense
-          fallback={<div className="text-5xl font-bold">Loading....</div>}
+          fallback={<div className="text-5xl font-bold">Loading...</div>}
         >
           {routes}
         </Suspense>
