33ef079c42c2ee2367d511beae537256661fb4df
Duc Nguyen <dnguyen416@gatech.edu>
2024-03-14 14:02:28 -0400

    added Discoverable Mode (draft)

--- client/src/pages/User.jsx
+++ client/src/pages/User.jsx
@@ -1,8 +1,24 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useAuth } from "../context/AuthContext";
+import DropDown from "../components/DropDown";
+import HorizontalScrollingMenu from "../components/HorizontalScrollingMenu";
+import axios from "axios";
 
 const User = () => {
+  const [isDiscoverable, setIsDiscoverable] = useState(false);
+  const [isPlanChosen, setIsPlanChosen] = useState(false);
   const { currentUser } = useAuth();
+  const [plans, setPlans] = useState([]);
+  useEffect(() => {
+    axios
+      .get(
+        `${import.meta.env.VITE_SERVER_URL}/api/user/${
+          currentUser.uid
+        }/plans/names`
+      )
+      .then((response) => setPlans(response.data.map((plan) => plan.name)))
+      .catch((error) => console.log(error));
+  }, []);
 
   return (
     <div className="flex flex-col justify-center items-center h-full gap-y-4">
@@ -12,11 +28,23 @@ const User = () => {
           <div className="card-actions justify-end">
             <label className="label cursor-pointer gap-4">
               <span className="label-text">Discoverable</span>
-              <input type="checkbox" className="toggle toggle-primary" />
+              <input
+                type="checkbox"
+                className="toggle toggle-primary"
+                checked={isDiscoverable}
+                onChange={() => {
+                  setIsDiscoverable(!isDiscoverable);
+                  setIsPlanChosen(false);
+                }}
+              />
             </label>
           </div>
         </div>
       </div>
+      {isDiscoverable && (
+        <DropDown options={plans} handleOptionSelect={setIsPlanChosen} />
+      )}
+      {isPlanChosen && <HorizontalScrollingMenu />}
     </div>
   );
 };
